e054752feac1979c66dd3022462803858f6d25e1
sebashack <sebastian0092@gmail.com>
2024-03-29 20:05:18 +0000

    Move tf_idf code to its own module

--- src/__main__.py
+++ src/__main__.py
@@ -1,64 +1,23 @@
+import os
 import uuid
 import sys
 import spacy
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import ArrayType, StringType
-from pyspark.ml.feature import CountVectorizer, IDF
-from pyspark.ml import Pipeline, PipelineModel
+
+from tf_idf import (
+    tokenize_tweet,
+    tokenize_dataset,
+    compute_tf_idf_model,
+    model_to_tf_idf,
+    load_tf_idf_model,
+)
 
 
-def tokenize_tweet(tweet, spacy_nlp):
-    doc = spacy_nlp(tweet.lower())
-    return [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
-
-
-def tokenize_dataset(df, spacy_nlp):
-    tweets_df = df.select("text")
-
-    tokenize_tweet_udf = udf(
-        lambda s: tokenize_tweet(s, spacy_nlp), ArrayType(StringType())
-    )
-    tweets_df = df.select("text")
-    tokenized_df = tweets_df.withColumn("tokenized_text", tokenize_tweet_udf("text"))
-
-    return tokenized_df
-
-
-def compute_tf_idf_model(tokenized_df, vocab_size):
-    cv = CountVectorizer(
-        inputCol="tokenized_text",
-        outputCol="tf_features",
-        vocabSize=vocab_size,
-        minDF=1.0,
-    )
-    idf = IDF(inputCol="tf_features", outputCol="tfidf_features")
-
-    pipeline = Pipeline(stages=[cv, idf])
-
-    model = pipeline.fit(tokenized_df)
-
-    return model
-
-
-def model_to_tf_idf(model, tokenized_df):
-    tfidf_df = model.transform(tokenized_df)
-
-    vocabulary = model.stages[0].vocabulary
-
-    return vocabulary, tfidf_df
-
-
-def load_tf_idf_model(path):
-    model = PipelineModel.load(path)
-
-    return model
-
-
-def main(argv):
+def tf_idf_pipeline():
     dataset_path = "/home/sebastian/Downloads/workshop2/climateTwitterData.csv/climateTwitterData.csv"
     spacy_nlp = spacy.load("en_core_web_sm")
     spark = SparkSession.builder.master("local[*]").getOrCreate()
+    spark.sparkContext.addPyFile(f"{os.getcwd()}/src/tf_idf.py")
 
     df = spark.read.csv(dataset_path, header=True, inferSchema=True)
 
@@ -79,4 +38,5 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    tf_idf_pipeline()
+    sys.exit(0)
---
+++ src/tf_idf.py
@@ -0,0 +1,51 @@
+from pyspark.sql.functions import udf
+from pyspark.sql.types import ArrayType, StringType
+from pyspark.ml.feature import CountVectorizer, IDF
+from pyspark.ml import Pipeline, PipelineModel
+
+
+def tokenize_tweet(tweet, spacy_nlp):
+    doc = spacy_nlp(tweet.lower())
+    return [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
+
+
+def tokenize_dataset(df, spacy_nlp):
+    tweets_df = df.select("text")
+
+    tokenize_tweet_udf = udf(
+        lambda s: tokenize_tweet(s, spacy_nlp), ArrayType(StringType())
+    )
+    tweets_df = df.select("text")
+    tokenized_df = tweets_df.withColumn("tokenized_text", tokenize_tweet_udf("text"))
+
+    return tokenized_df
+
+
+def compute_tf_idf_model(tokenized_df, vocab_size):
+    cv = CountVectorizer(
+        inputCol="tokenized_text",
+        outputCol="tf_features",
+        vocabSize=vocab_size,
+        minDF=1.0,
+    )
+    idf = IDF(inputCol="tf_features", outputCol="tfidf_features")
+
+    pipeline = Pipeline(stages=[cv, idf])
+
+    model = pipeline.fit(tokenized_df)
+
+    return model
+
+
+def model_to_tf_idf(model, tokenized_df):
+    tfidf_df = model.transform(tokenized_df)
+
+    vocabulary = model.stages[0].vocabulary
+
+    return vocabulary, tfidf_df
+
+
+def load_tf_idf_model(path):
+    model = PipelineModel.load(path)
+
+    return model
