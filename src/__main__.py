7764fcae5f7d86c7983cc828a9349f7dd627a484
nerthust <danilotoro978@gmail.com>
2024-03-31 12:43:34 -0500

    Define topics search

--- src/__main__.py
+++ src/__main__.py
@@ -13,6 +13,10 @@ from embeddings import (
     embeddings_pipeline_with_saved_response,
 )
 
+from topic_modelling import (
+    topic_modelling_pipeline,
+    get_model_lda_and_k_optimal,
+)
 
 def pipeline1(df):
     spacy_nlp = spacy.load("en_core_web_sm")
@@ -28,16 +32,23 @@ def pipeline2(df):
     #     df, 3000, "embeddings__2024-03-29_22-02-54.json", "./chromadb"
     # )
 
+def pipeline3(df):
+    spacy_nlp = spacy.load("en_core_web_sm")
+    lda_model = topic_modelling_pipeline(spacy_nlp, df, 5, 0.5, 1000, 3, 10, 10)
+    lda_model.print_topics(-1)
+    # lda_model, k_optimal = get_model_lda_and_k_optimal(spacy_nlp, df,  5, 0.5, 1000, 3, 10, 10, 'u_mass')
+
 
 def main():
-    dataset_path = "/home/sebastian/Downloads/workshop2/climateTwitterData.csv/climateTwitterData.csv"
+    dataset_path = "/home/nerthust/Downloads/climateTwitterData.csv"
     spark = SparkSession.builder.master("local[*]").getOrCreate()
     spark.sparkContext.addPyFile(f"{os.getcwd()}/src/tf_idf.py")
 
     df = spark.read.csv(dataset_path, header=True, inferSchema=True)
 
     # pipeline1(df)
-    pipeline2(df)
+    # pipeline2(df)
+    pipeline3(df)
 
 
 if __name__ == "__main__":
